515cdd3ec709ad148f86b48fc0cde3cc291c01cc
jasenfinch <jsf9@aber.ac.uk>
2017-07-27 16:46:25 +0100

    added documentation for retry workflow

--- R/retryWorkflow.R
+++ R/retryWorkflow.R
@@ -1,3 +1,7 @@
+#' retryWorkflow
+#' @description Retry a workflow after initial analysis with \code{workflow} following an error. 
+#' Will resume the analysis from the point of error. 
+#' @param analysis an S4 object of class Workflow
 #' @export
 
 retryWorkflow <- function(analysis){
