de79fecb0d145ca60cf2a1b39e9f602adb05c45d
jasenfinch <jsf9@aber.ac.uk>
2017-08-08 16:14:16 +0100

    added RP-LCMS workflow

--- R/workflowParameters.R
+++ R/workflowParameters.R
@@ -2,27 +2,40 @@
 #' @description Initiate default workflow parameters for a selected workflow.
 #' @param workflow the workflow analysis to use. NULL prints the available workflows.
 #' @importFrom binneRlyse binParameters
+#' @importFrom profilePro profileParameters
 #' @importFrom metabolyseR analysisParameters
 #' @importFrom MFassign assignmentParameters
 #' @export
 
 workflowParameters <- function(workflow = NULL){
-  availWorkflows <- c('FIE_HRMSfingerprinting','FIE_HRMSfingerprinting2')
+  availWorkflows <- c('FIE_HRMSfingerprinting','FIE_HRMSfingerprinting2','RP_LC_HRMSprofiling')
   if (is.null(workflow)) {
     availWorkflows <- paste(availWorkflows,collapse = '\n\t\t\t')
     availWorkflows <- paste('\n\t\t\t',availWorkflows,sep = '')
     cat('\nAvailable Workflows:',availWorkflows,sep = '')
   } else {
     if (workflow %in% availWorkflows) {
+      
       if (grepl('FIE',workflow)) {
         w <- 'FIE'
+        new('WorkflowParameters',
+            workflow = workflow,
+            processing = binParameters(),
+            analysis = analysisParameters(),
+            annotation = assignmentParameters(w)
+        )
       }
-      new('WorkflowParameters',
-          workflow = workflow,
-          processing = binParameters(),
-          analysis = analysisParameters(),
-          annotation = assignmentParameters(w)
-      )
+      
+      if (grepl('RP_LC_HRMS',workflow)) {
+        w <- 'FIE'
+        new('WorkflowParameters',
+            workflow = workflow,
+            processing = profileParameters('LCMS-RP'),
+            analysis = analysisParameters(),
+            annotation = assignmentParameters(w)
+        )
+      }
+      
     }
   }
 }
