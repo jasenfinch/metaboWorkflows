5aa7e8b845871fa25ec0e8ba4d0c2c539298ca3c
jasenfinch <jsf9@aber.ac.uk>
2018-02-08 19:38:51 +0000

    added console output to FIE-HRMS workflows

--- R/FIE_HRMSfingerprinting.R
+++ R/FIE_HRMSfingerprinting.R
@@ -2,16 +2,21 @@
 #' @importFrom metabolyseR analysisParameters metabolyse reAnalyse
 #' @importFrom dplyr bind_cols
 #' @importFrom binneRlyse info
+#' @importFrom cli symbol
+#' @importFrom crayon green
 
 FIE_HRMSfingerprinting <- function(elements = NULL){
   methods <- list(
     spectralBin = function(x){
+      cat('\nSpectral binning',cli::symbol$continue,'\r')
       binnedDat <- binneRlyse(files = unlist(x@files),info = x@info,parameters = x@workflowParameters@processing)
       x@processed <- binnedDat
+      cat('\rSpectral binning',green(cli::symbol$tick),'\n')
       return(x)
     },
     
     preTreat = function(x){
+      cat('\nPre-treatment',cli::symbol$continue,'\r')
       preTreatParameters <- analysisParameters('preTreat')
       preTreatParameters@preTreat <- x@workflowParameters@analysis@preTreat
       
@@ -29,35 +34,42 @@ FIE_HRMSfingerprinting <- function(elements = NULL){
                         featureSelection = tibble(),
                         correlations = tibble()
       )
+      cat('\rPre-treatment',green(cli::symbol$tick),'\n')
       return(x)
     },
     
     dataQualityCheckPoint = function(x){
-      cat('\nData pre-treatment complete. Break point for data quality check. Use restartWorkflow() to continue analysis.\n\n') 
+      cat(blue('\nBreak point for data quality check. Use restartWorkflow() to continue analysis.\n') )
       return(x)
     },
     
     classification = function(x){
+      cat('\nClassification',cli::symbol$continue,'\r')
       p <- analysisParameters('classification')
       p@classification <- x@workflowParameters@analysis@classification
       x@analysed <- reAnalyse(x@analysed,p) 
       x@analysed@parameters <- x@workflowParameters@analysis
+      cat('\rClassification',green(cli::symbol$tick),'\n')
       return(x)
     },
     
     featureSelection = function(x){
+      cat('\nFeature selection',cli::symbol$continue,'\r')
       p <- analysisParameters('featureSelection')
       p@featureSelection <- x@workflowParameters@analysis@featureSelection
       x@analysed <- reAnalyse(x@analysed,p) 
       x@analysed@parameters <- x@workflowParameters@analysis
+      cat('\rFeature selection',green(cli::symbol$tick),'\n')
       return(x)
     },
     
     correlations = function(x){
+      cat('\nCorrelations',cli::symbol$continue,'\r')
       p <- analysisParameters('correlations')
       p@correlations <- x@workflowParameters@analysis@correlations
       x@analysed <- reAnalyse(x@analysed,p) 
       x@analysed@parameters <- x@workflowParameters@analysis
+      cat('\rCorrelations',green(cli::symbol$tick),'\n')
       return(x)
     }
   )
