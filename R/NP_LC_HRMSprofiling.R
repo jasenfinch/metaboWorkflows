a1a9fe200302231e9d5dbef16177583df04fb3e6
jasenfinch <jsf9@aber.ac.uk>
2019-06-11 19:44:45 +0100

    added MF assignment to NP and RP workflows

--- R/NP_LC_HRMSprofiling.R
+++ R/NP_LC_HRMSprofiling.R
@@ -2,7 +2,3 @@
 NP_LC_HRMSprofiling <- function(elements = NULL){
   RP_LC_HRMSprofiling(elements)
 }
-
-NP_LC_HRMSprofiling2 <- function(elements = NULL){
-  RP_LC_HRMSprofiling2(elements)
-}
--- R/RP_LC_HRMSprofiling.R
+++ R/RP_LC_HRMSprofiling.R
@@ -10,39 +10,13 @@ RP_LC_HRMSprofiling <- function(elements = NULL){
       return(x)
     },
     
-    preTreat = function(x){
-      preTreatParameters <- analysisParameters('preTreat')
-      preTreatParameters@preTreat <- x@workflowParameters@analysis@preTreat
-      
-      modes <- names(x@processed@Data)
-      
-      pt <- map(modes,~{
-        metabolyse(x@processed@Data[[.]],x@processed@Info,preTreatParameters)
-      })
-      names(pt) <- modes
-      
-      dat <- map(pt,~{
-        .@preTreated$Data
-      }) %>%
-        bind_cols()
-      
-      info <- pt$neg@preTreated$Info
-      x@analysed <- new('Analysis',
-                        log = list(analysis = date()),
-                        parameters = x@workflowParameters@analysis,
-                        rawData = list(),
-                        preTreated = list(Data = dat,Info = info),
-                        classification = tibble(),
-                        featureSelection = tibble(),
-                        correlations = tibble()
-      )
-      return(x)
-    },
+    preTreat = FIE_HRMSfingerprinting('preTreat'),
     
-    dataQualityCheckPoint = function(x){
-      cat('\nData pre-treatment complete. Break point for data quality check. Use restartWorkflow() to continue analysis.\n\n') 
-      return(x)
-    },
+    dataQualityCheckPoint = FIE_HRMSfingerprinting('dataQualityCheckPoint'),
+    
+    MFassignment = FIE_HRMSfingerprinting('MFassignment'),
+    
+    MFassignmentCheckPoint = FIE_HRMSfingerprinting('MFassignmentCheckPoint'),
     
     classification = function(x){
       p <- analysisParameters('classification')
@@ -52,49 +26,9 @@ RP_LC_HRMSprofiling <- function(elements = NULL){
       return(x)
     },
     
-    featureSelection = function(x){
-      p <- analysisParameters('featureSelection')
-      p@featureSelection <- x@workflowParameters@analysis@featureSelection
-      x@analysed <- reAnalyse(x@analysed,p)
-      x@analysed@parameters <- x@workflowParameters@analysis
-      return(x)
-    },
+    featureSelection = FIE_HRMSfingerprinting('featureSelection'),
     
-    correlations = function(x){
-      p <- analysisParameters('correlations')
-      p@correlations <- x@workflowParameters@analysis@correlations
-      x@analysed <- reAnalyse(x@analysed,p)
-      x@analysed@parameters <- x@workflowParameters@analysis
-      return(x)
-    }
-  )
-  
-  if (!is.null(elements)) {
-    methods <- methods[[elements]]
-  }
-  
-  return(methods)
-}
-
-RP_LC_HRMSprofiling2 <- function(elements = NULL){
-  methods <- list(
-    peakPick = RP_LC_HRMSprofiling('peakPick'),
-    
-    preTreat = RP_LC_HRMSprofiling('preTreat'),
-    
-    dataQualityCheckPoint = RP_LC_HRMSprofiling('dataQualityCheckPoint'),
-    
-    correlations1 =  RP_LC_HRMSprofiling('correlations'),
-    
-    annotation = FIE_HRMSfingerprinting2('annotation'),
-    
-    MFassignmentCheckPoint = FIE_HRMSfingerprinting2('MFassignmentCheckPoint'),
-    
-    classification = RP_LC_HRMSprofiling('classification'),
-    
-    featureSelection = RP_LC_HRMSprofiling('featureSelection'),
-    
-    correlations = RP_LC_HRMSprofiling('correlations')
+    correlations = FIE_HRMSfingerprinting('correlations')
   )
   
   if (!is.null(elements)) {
