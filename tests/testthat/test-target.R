ff3b56a86f1ea1ed029c3018372193a2035c1125
jasenfinch <jsf9@aber.ac.uk>
2021-10-05 11:47:29 +0100

    fixed target tests

--- tests/testthat/test-target.R
+++ tests/testthat/test-target.R
@@ -1,20 +1,20 @@
 test_that("target defintion works", {
-  workflow_target <- target('a_target','1 + 1')
+  workflow_target <- target('a_target',1 + 1)
   
   expect_s4_class(workflow_target,'Target')
   expect_output(print(workflow_target),'tar_target')
 })
 
 test_that('invalid targets are correctly detected',{
-  expect_error(target('a target','1 + 1'))
-  expect_error(target('a_target','1 + 1',type = 'wrong'))
+  expect_error(target('a target',1 + 1))
+  expect_error(target('a_target',1 + 1,type = 'wrong'))
 })
 
 test_that('target elements can be returned',{
-  workflow_target <- target('a_target','1 + 1')
+  workflow_target <- target('a_target',1 + 1)
   
   expect_identical(name(workflow_target),'a_target')
-  expect_identical(command(workflow_target),'1 + 1')
+  expect_identical(command(workflow_target),rlang::exprs(1 + 1))
   expect_identical(type(workflow_target),'tar_target')
   expect_identical(args(workflow_target),list())
   expect_identical(comment(workflow_target),character())
@@ -22,16 +22,16 @@ test_that('target elements can be returned',{
 })
 
 test_that('target elements can be set',{
-  workflow_target <- target('a_target','1 + 1')
+  workflow_target <- target('a_target',1 + 1)
   
   name(workflow_target) <- 'new_target'
-  command(workflow_target) <- '2 + 2'
+  command(workflow_target) <- rlang::expr(2 + 2)
   type(workflow_target) <- 'tar_file'
   args(workflow_target) <- list(format = 'file')
   comment(workflow_target) <- 'a target'
   
   expect_identical(name(workflow_target),'new_target')
-  expect_identical(command(workflow_target),'2 + 2')
+  expect_identical(command(workflow_target),rlang::exprs(2 + 2))
   expect_identical(type(workflow_target),'tar_file')
   expect_identical(args(workflow_target),list(format = 'file'))
   expect_identical(comment(workflow_target),'a target')
