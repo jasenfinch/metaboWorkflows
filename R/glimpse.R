4de2245e2db049ef4f3a26d6a6e3420f63d37348
jasenfinch <jsf9@aber.ac.uk>
2021-11-12 09:15:01 +0000

    fixed glimpse

--- R/glimpse.R
+++ R/glimpse.R
@@ -1,3 +1,51 @@
+#' @importFrom purrr map
+#' @importFrom glue glue_collapse
+
+targetsList <- function(workflow_targets){
+  
+  workflow_targets <- workflow_targets %>%
+    map(~{
+      wt <- .x
+      wt %>%
+        names() %>%
+        map(~{
+          target_code <- code(wt[[.x]])
+          
+          if (length(target_code) > 1){
+            target_code <- glue_collapse(target_code,sep = '
+                                         ')
+          }
+          
+          if (length(comment(wt[[.x]])) > 0) {
+            glue('
+{.x} = 
+       {target_code}')
+          } else {
+            glue('
+{.x} = {target_code}')
+          }
+        }) %>%
+        glue_collapse(sep = ',
+')
+    })
+  
+  workflow_targets <- workflow_targets %>% 
+    names() %>% 
+    map_chr(~{
+      glue('{.x} = list(
+    {workflow_targets[[.x]]}
+  )')
+    }) %>% 
+    glue_collapse(sep = ',
+')
+  
+  workflow_targets <- glue('list(
+  {workflow_targets}
+)')
+  
+  return(workflow_targets)
+}
+
 #' Visualise the workflow targets
 #' @rdname glimpse
 #' @description Visualise the directed acyclic graph for a workflow's targets.
