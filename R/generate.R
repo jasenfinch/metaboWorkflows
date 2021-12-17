9c7f07dec65a7cfc976e2401447b5409c8d2d652
jasenfinch <jsf9@aber.ac.uk>
2021-12-17 12:35:02 +0000

    added script header to generated R/utils.R file

--- R/generate.R
+++ R/generate.R
@@ -52,6 +52,7 @@ setMethod('generateWorkflow',signature = 'Workflow',
             utils(glue('{project_directory}/R'),
                   cran = c('purrr','targets','tarchetypes')
             )
+            editHeader(paste0(project_directory,'/R/utils.R'))
             
             inputPrep(workflow)
             
