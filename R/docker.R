f83e7e300df815ade186a9c9c780800d3001801f
jasenfinch <jsf9@aber.ac.uk>
2023-01-17 09:05:03 +0000

    Docker infrastructure now generated using the latest tag if the hrm-docker package infrastructure cannot be accessed

--- R/docker.R
+++ R/docker.R
@@ -2,15 +2,31 @@
 #' @importFrom purrr flatten_chr
 
 dockerImage <- function(project_directory){
-  latest_hrm_docker_tag <- gh('GET /users/{username}/packages/{package_type}/{package_name}/versions',
-                            username = 'jasenfinch',
-                            package_type = 'container',
-                            package_name = 'hrm-docker') %>% 
-    map(~{.x$metadata$container$tags}) %>% 
-    flatten() %>% 
-    flatten_chr() %>% 
-    sort(decreasing = TRUE) %>% 
-    .[2]
+  latest_hrm_docker_tag <- try(gh('GET /users/{username}/packages/{package_type}/{package_name}/versions',
+                                  username = 'jasenfinch',
+                                  package_type = 'container',
+                                  package_name = 'hrm-docker'),
+                               silent = TRUE) 
+  
+  if (!inherits(latest_hrm_docker_tag,'try-error')){
+    latest_hrm_docker_tag <- latest_hrm_docker_tag %>% 
+      map(~{.x$metadata$container$tags}) %>%
+      flatten() %>%
+      flatten_chr() %>%
+      sort(decreasing = TRUE) %>%
+      .[2]
+  }
+  
+  if (inherits(latest_hrm_docker_tag,'try-error')){
+    warning(paste(
+      'Unable to access the package information for the hrm-docker image at <https://github.com/jasenfinch/hrm-docker/pkgs/container/hrm-docker>.',
+      'Resorting to the `latest` tag but this will be unreproducible in the long-term',
+      'Please consider changing this manually in the generated project.',
+      'The returned GitHub API query error was:\n',
+      latest_hrm_docker_tag[1]))
+    
+    latest_hrm_docker_tag <- 'latest'
+  }
   
   dockerfile_path <- paste0(project_directory,'/misc/docker/Dockerfile')
   dockerfile <- readLines(dockerfile_path)
--- tests/testthat/test-generate.R
+++ tests/testthat/test-generate.R
@@ -52,3 +52,35 @@ test_that('generateWorkflow works for grover API input',{
   
   unlink(paste0(temp_dir,'/Example_project'),recursive = TRUE)
 })
+
+test_that('aproject can be generated with Docker infrastructure but no GitHub API token',{
+  temp_dir <- tempdir()
+  gh_token <- Sys.getenv('GITHUB_PAT')
+  Sys.setenv(GITHUB_PAT = '')
+  
+  if (dir.exists(paste0(temp_dir,'/Example_project'))){
+    unlink(paste0(temp_dir,'/Example_project'),recursive = TRUE)
+  }
+  
+  workflow_input <- inputGrover('an_instrument',
+                                'a_directory',
+                                'a.grover.host',
+                                80,
+                                '1234')
+  
+  workflow_definition <- defineWorkflow(workflow_input,
+                                        'FIE-HRMS fingerprinting',
+                                        'Example project',
+                                        path = temp_dir,
+                                        renv = FALSE,
+                                        force = TRUE
+  )
+  
+  expect_warning(generateWorkflow(workflow_definition,start = FALSE))
+  
+  expect_true(dir.exists(paste0(temp_dir,'/Example_project')))
+  expect_true(grepl('latest',readLines(paste0(temp_dir,'/Example_project/misc/docker/Dockerfile'))[3]))
+  
+  unlink(paste0(temp_dir,'/Example_project'),recursive = TRUE)
+  Sys.setenv(GITHUB_PAT = gh_token)
+})
